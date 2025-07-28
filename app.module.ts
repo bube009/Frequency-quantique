97ee4db9eced3af809161322af9d2eb0002b1d4d
bube009 <207951757+bube009@users.noreply.github.com>
2025-07-28 00:21:33 -0400

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicModule } from '@ionic/angular';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
